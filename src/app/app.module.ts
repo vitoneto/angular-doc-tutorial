243b1fe7805540ce66db2c8b0750a618fd4fc043
vitoneto <vito.neto@sistemasigma.com>
2022-10-07 13:32:30 -0300

    https://angular.io/start/start-routing all done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,27 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+	imports: [
+		BrowserModule,
+		ReactiveFormsModule,
+		RouterModule.forRoot([
+			{ path: '', component: ProductListComponent },
+			{ path: 'products/:productId', component: ProductDetailsComponent }
+		])
+	],
+	declarations: [
+		AppComponent,
+		TopBarComponent,
+		ProductListComponent,
+		ProductAlertsComponent,
+		ProductDetailsComponent
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
