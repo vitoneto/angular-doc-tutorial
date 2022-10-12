bf12a0b99ece815dbdd09d034aaf0911296a4fb8
vitoneto <vito.neto@sistemasigma.com>
2022-10-12 18:02:04 -0300

    /start/start-data#create-a-shipping-component done

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -23,7 +23,7 @@ export class CartService {
 		return this.items;
 	}
 
-	getShippingPrice() {
+	getShippingPrices() {
 		return this.http.get<{ type: string; price: number }[]>(
 			'/assets/shipping.json'
 		);
--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from '../cart.service';
 
 @Component({
-  selector: 'app-shipping',
-  templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.css']
+	selector: 'app-shipping',
+	templateUrl: './shipping.component.html',
+	styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
+	shippingCosts!: Observable<{ type: string; price: number }[]>;
 
-  constructor() { }
+	constructor(private cartService: CartService) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.shippingCosts = this.cartService.getShippingPrices();
+	}
 }
