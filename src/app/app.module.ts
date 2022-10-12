63732faf21e49114dbfcbd7722daebc9796b7ec8
vitoneto <vito.neto@sistemasigma.com>
2022-10-12 17:42:26 -0300

    Create a shipping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
 	imports: [
@@ -19,7 +20,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 		RouterModule.forRoot([
 			{ path: '', component: ProductListComponent },
 			{ path: 'products/:productId', component: ProductDetailsComponent },
-			{ path: 'cart', component: CartComponent }
+			{ path: 'cart', component: CartComponent },
+			{ path: 'shipping', component: ShippingComponent }
 		])
 	],
 	declarations: [
@@ -28,7 +30,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 		ProductListComponent,
 		ProductAlertsComponent,
 		ProductDetailsComponent,
-		CartComponent
+		CartComponent,
+		ShippingComponent
 	],
 	bootstrap: [AppComponent]
 })
